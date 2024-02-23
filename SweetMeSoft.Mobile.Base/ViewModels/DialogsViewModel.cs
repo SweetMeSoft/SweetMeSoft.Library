da5ec1aee1011379758de6014056aa7a6c7b10ef
Erick Velasco <erickvelasco11@gmail.com>
2024-02-23 00:54:38 -0500

    Added main thread to DisplayDialogs

--- SweetMeSoft.Mobile.Base/ViewModels/DialogsViewModel.cs
+++ SweetMeSoft.Mobile.Base/ViewModels/DialogsViewModel.cs
@@ -17,11 +17,39 @@ public class DialogsViewModel : BaseViewModel
 
     public async Task<bool> DisplayAlert(string title, string message, string okText, string cancelText)
     {
-        return await UserDialogs.Instance.ConfirmAsync(message, title, okText, cancelText);
+        var tcs = new TaskCompletionSource<bool>();
+        Application.Current?.Dispatcher.Dispatch(async () =>
+        {
+            try
+            {
+                var t = await UserDialogs.Instance.ConfirmAsync(message, title, okText, cancelText);
+                tcs.SetResult(t);
+            }
+            catch (Exception e)
+            {
+                tcs.SetException(e);
+            }
+        });
+
+        return await tcs.Task;
     }
 
     public async Task<string> DisplayPrompt(string title, string message, string accept = "Ok", string cancel = "Cancel", Keyboard keyboard = null, string initial = "")
     {
-        return await Application.Current.MainPage.DisplayPromptAsync(title, message, accept, cancel, "", 50, keyboard, initial);
+        var tcs = new TaskCompletionSource<string>();
+        Application.Current?.Dispatcher.Dispatch(async () =>
+        {
+            try
+            {
+                var t = await Application.Current.MainPage.DisplayPromptAsync(title, message, accept, cancel, "", 50, keyboard, initial);
+                tcs.SetResult(t);
+            }
+            catch (Exception e)
+            {
+                tcs.SetException(e);
+            }
+        });
+
+        return await tcs.Task;
     }
 }
