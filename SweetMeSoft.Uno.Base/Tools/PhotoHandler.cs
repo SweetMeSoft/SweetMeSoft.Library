c2987455508a3ef4996b57740e5291ae38e0379c
Erick Velasco <erickvelasco11@gmail.com>
2024-05-14 20:49:27 -0500

    Fix iOS take photo

--- SweetMeSoft.Uno.Base/Tools/PhotoHandler.cs
+++ SweetMeSoft.Uno.Base/Tools/PhotoHandler.cs
@@ -4,24 +4,18 @@ using System.Drawing;
 
 using Windows.Devices.Sensors;
 using Windows.Media.Capture;
+using Windows.Storage;
 
 namespace SweetMeSoft.Uno.Base.Tools;
 
 public class PhotoHandler
 {
     //TODO Storage the new photo in a temp folder
-    public static async Task<PhotoResult> TakePhoto()
+    public async Task<PhotoResult> TakePhoto()
     {
         try
         {
-            var captureUI = new CameraCaptureUI();
-            captureUI.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
-            //captureUI.PhotoSettings.CroppedSizeInPixels = new Size(400, 400);
-            //captureUI.PhotoSettings.MaxResolution = CameraCaptureUIMaxPhotoResolution.SmallVga;
-            //captureUI.PhotoSettings.AllowCropping = true;
-            //captureUI.PhotoSettings.CroppedAspectRatio = new Size(1, 1);
-
-            var photo = await captureUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
+            var photo = await capturer.CaptureFileAsync(CameraCaptureUIMode.Photo);
             if (photo == null)
             {
                 return null;
@@ -29,10 +23,16 @@ public class PhotoHandler
             else
             {
                 var orientation = SimpleOrientationSensor.GetDefault()?.GetCurrentOrientation() ?? SimpleOrientation.NotRotated;
+                var bytes = GetFixedPhoto(photo.Path, orientation);
+                var newPath = Path.Combine(ApplicationData.Current.TemporaryFolder.Path, Guid.NewGuid().ToString());
+
+                File.Delete(photo.Path);
+                File.WriteAllBytes(newPath, bytes);
+
                 return new PhotoResult
                 {
-                    OriginalPath = photo.Path,
-                    EditedBytes = GetFixedPhoto(photo.Path, orientation),
+                    OriginalPath = newPath,
+                    EditedBytes = bytes,
                     Orientation = orientation
                 };
             }
@@ -45,7 +45,7 @@ public class PhotoHandler
         return null;
     }
 
-    public static async Task<List<PhotoResult>> TakePhotos()
+    public async Task<List<PhotoResult>> TakePhotos()
     {
         var results = new List<PhotoResult>();
         PhotoResult photo;
@@ -61,14 +61,14 @@ public class PhotoHandler
         return results;
     }
 
-    private static byte[] GetFixedPhoto(string path, SimpleOrientation orientation)
+    private byte[] GetFixedPhoto(string path, SimpleOrientation orientation)
     {
         using var sourceStream = File.OpenRead(path);
         using var memoryStream = new MemoryStream();
         sourceStream.CopyTo(memoryStream);
         var bytes = memoryStream.ToArray();
 
-        SKBitmap image = SKBitmap.Decode(bytes);
+        using SKBitmap image = SKBitmap.Decode(bytes);
         if (orientation == SimpleOrientation.NotRotated && image.Width > image.Height)
         {
             bytes = RotateImage(bytes, 90);
@@ -94,29 +94,28 @@ public class PhotoHandler
         return ScaleImage(bytes, newSize.Width, newSize.Height);
     }
 
-    private static Size GetSized(byte[] bytes, int maxSide)
+    private Size GetSized(byte[] bytes, int maxSide)
     {
-        SKBitmap originalImage = SKBitmap.Decode(bytes);
-        if (originalImage.Height > originalImage.Width)
+        using SKBitmap bitmap = SKBitmap.Decode(bytes);
+        if (bitmap.Height > bitmap.Width)
         {
-            return new Size(originalImage.Width * maxSide / originalImage.Height, maxSide);
+            return new Size(bitmap.Width * maxSide / bitmap.Height, maxSide);
         }
         else
         {
-            if (originalImage.Height < originalImage.Width)
+            if (bitmap.Height < bitmap.Width)
             {
-                return new Size(maxSide, originalImage.Height * maxSide / originalImage.Width);
+                return new Size(maxSide, bitmap.Height * maxSide / bitmap.Width);
             }
 
             return new Size(maxSide, maxSide);
         }
     }
 
-    private static byte[] RotateImage(byte[] bytes, int degrees)
+    private byte[] RotateImage(byte[] bytes, int degrees)
     {
-        using var bitmap = SKBitmap.Decode(bytes);
-        var rotated = new SKBitmap(bitmap.Height, bitmap.Width);
-
+        using SKBitmap bitmap = SKBitmap.Decode(bytes);
+        using var rotated = new SKBitmap(bitmap.Height, bitmap.Width);
         using (var surface = new SKCanvas(rotated))
         {
             surface.Translate(rotated.Width, 0);
@@ -124,22 +123,25 @@ public class PhotoHandler
             surface.DrawBitmap(bitmap, 0, 0);
         }
 
-        return rotated.Encode(SKEncodedImageFormat.Jpeg, 100).ToArray();
+        return rotated.Encode(SKEncodedImageFormat.Jpeg, 80).ToArray();
     }
 
-    private static byte[] ScaleImage(byte[] bytes, int maxWidth, int maxHeight)
+    private byte[] ScaleImage(byte[] bytes, int maxWidth, int maxHeight)
     {
-        SKBitmap image = SKBitmap.Decode(bytes);
-
-        var ratioX = (double)maxWidth / image.Width;
-        var ratioY = (double)maxHeight / image.Height;
+        using SKBitmap bitmap = SKBitmap.Decode(bytes);
+        var ratioX = (double)maxWidth / bitmap.Width;
+        var ratioY = (double)maxHeight / bitmap.Height;
         var ratio = Math.Min(ratioX, ratioY);
-
-        var newWidth = (int)(image.Width * ratio);
-        var newHeight = (int)(image.Height * ratio);
-
+        var newWidth = (int)(bitmap.Width * ratio);
+        var newHeight = (int)(bitmap.Height * ratio);
         var info = new SKImageInfo(newWidth, newHeight);
-        image = image.Resize(info, SKFilterQuality.High);
-        return image.Encode(SKEncodedImageFormat.Jpeg, 100).ToArray();
+        using var resized = bitmap.Resize(info, SKFilterQuality.High);
+        return resized.Encode(SKEncodedImageFormat.Jpeg, 80).ToArray();
     }
+
+    public static PhotoHandler Instance => instance ??= new PhotoHandler();
+
+    private static PhotoHandler instance;
+
+    private readonly CameraCaptureUI capturer = new CameraCaptureUI();
 }
