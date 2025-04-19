08778de72514d6d5701b982b54ad1b0fea523bcd
Erick Velasco <erickvelasco11@gmail.com>
2025-04-19 16:16:14 -0500

    Fix gray error

--- SweetMeSoft.Mobile.Base/ViewModels/DialogsViewModel.cs
+++ SweetMeSoft.Mobile.Base/ViewModels/DialogsViewModel.cs
@@ -2,17 +2,21 @@
 
 using Controls.UserDialogs.Maui;
 
+using SweetMeSoft.Mobile.Base.Popup;
+
 namespace SweetMeSoft.Mobile.Base.ViewModels;
 
 public class DialogsViewModel : ObservableObject
 {
     public Task DisplayAlert(string title, string message, string cancel)
     {
+        PopupsService.Instance.HideLoading();
         return UserDialogs.Instance.AlertAsync(message, title, cancel);
     }
 
     public Task<bool> DisplayAlert(string title, string message, string okText, string cancelText)
     {
+        PopupsService.Instance.HideLoading();
         return UserDialogs.Instance.ConfirmAsync(message, title, okText, cancelText);
         //var tcs = new TaskCompletionSource<bool>();
         //Application.Current?.Dispatcher.Dispatch(async () =>
@@ -33,6 +37,7 @@ public class DialogsViewModel : ObservableObject
 
     public async Task<string> DisplayPrompt(string title, string message, string accept = "Ok", string cancel = "Cancel", Keyboard keyboard = null, string initial = "")
     {
+        PopupsService.Instance.HideLoading();
         var tcs = new TaskCompletionSource<string>();
         Application.Current?.Dispatcher.Dispatch(async () =>
         {
@@ -52,6 +57,7 @@ public class DialogsViewModel : ObservableObject
 
     public async Task DisplaySnackbar(string message, Action action = null)
     {
+        PopupsService.Instance.HideLoading();
         var result = await UserDialogs.Instance.ShowSnackbarAsync(message, actionText: action != null ? "Ok" : null, showCountDown: action != null);
         if (action != null)
         {
@@ -64,6 +70,7 @@ public class DialogsViewModel : ObservableObject
 
     public void DisplayToast(string message, Action action = null)
     {
+        PopupsService.Instance.HideLoading();
         UserDialogs.Instance.ShowToast(message);
     }
 }
